a00e91f4320ad5468eff85af542f1cdf0cb0340f
haslersn <sebastian.hasler@gmx.net>
2020-06-23 03:02:32 +0200

    Test: Add tests for (inverse) CRT

--- src/test.rs
+++ src/test.rs
@@ -1,7 +1,12 @@
+use crate::poly::*;
 use crate::residue::*;
 use rand::thread_rng;
 use rand::Rng;
 use std::iter::{once, repeat};
+
+fn random_residues<'a, R: Rng>(rng: &'a mut R) -> impl Iterator<Item = Residue> + 'a {
+    repeat(()).map(move |_| rng.gen())
+}
 
 fn example_residues<'a, R: Rng>(rng: &'a mut R) -> impl Iterator<Item = Residue> + 'a {
     once(Residue::zero())
@@ -9,7 +14,7 @@ fn example_residues<'a, R: Rng>(rng: &'a mut R) -> impl Iterator<Item = Residue>
         .chain(once(INDEX_TH_ROOT))
         .chain(once(INV_INDEX_TH_ROOT))
         .chain(once(INV_INDEX_TH_ROOT_MINUS_ONE))
-        .chain(repeat(()).map(move |_| rng.gen()))
+        .chain(random_residues(rng))
 }
 
 #[test]
@@ -61,4 +66,32 @@ fn test_inv_index_th_root_minus_one() {
         INV_INDEX_TH_ROOT_MINUS_ONE * (INDEX_TH_ROOT - Residue::one()),
         Residue::one()
     );
+}
+
+#[test]
+fn test_crt_roundtrip() {
+    let mut rng = thread_rng();
+    for _ in 0..3 {
+        let mut p1 = PowerPoly(Box::new([Residue::zero(); DEGREE]));
+        for (d, s) in p1.0.iter_mut().zip(random_residues(&mut rng)) {
+            *d = s;
+        }
+        let c = ChremPoly::from(p1.clone());
+        let p2 = PowerPoly::from(c);
+        assert_eq!(p1, p2);
+    }
+}
+
+#[test]
+fn test_inv_crt_roundtrip() {
+    let mut rng = thread_rng();
+    for _ in 0..3 {
+        let mut c1 = ChremPoly(Box::new([Residue::zero(); DEGREE]));
+        for (d, s) in c1.0.iter_mut().zip(random_residues(&mut rng)) {
+            *d = s;
+        }
+        let p = PowerPoly::from(c1.clone());
+        let c2 = ChremPoly::from(p);
+        assert_eq!(c1, c2);
+    }
 }
